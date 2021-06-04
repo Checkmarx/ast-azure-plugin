0fa6134b953cc1fb56655058f14ac5c57b506169
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-06-04 19:35:31 -0400

    initial scan create working prototype

--- CxASTScan/services/TaskRunner.ts
+++ CxASTScan/services/TaskRunner.ts
@@ -1,10 +1,27 @@
 import {factory} from "./ConfigLog4j";
 import taskLib = require('azure-pipelines-task-lib/task');
-
+import {CxScanConfigCall} from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxScanConfigCall"
+import {CxAuthCall} from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxAuthCall"
+import {CxParamType} from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxParamType";
 export class TaskRunner {
     private readonly log = factory.getLogger("TaskRunner");
+    private cxScanConfig = new CxScanConfigCall();
     async run() {
         this.printHeader();
+        this.cxScanConfig = this.initiateScanConfig();
+        let params: Map<CxParamType,string> = new Map<CxParamType,string>();
+        params.set(CxParamType.PROJECT_NAME,taskLib.getInput("projectName"));
+        params.set(CxParamType.SCAN_TYPES,"sast");
+        if(taskLib.getInput("syncMode")) {
+            params.set(CxParamType.ADDITIONAL_PARAMETERS, "--nowait");
+        }
+        params.set(CxParamType.SAST_PRESET_NAME,"Checkmarx Default");
+        params.set(CxParamType.S,".");
+        const auth = new CxAuthCall(this.cxScanConfig);
+        let result = await auth.scanCreate(params).then(value => {
+            console.log(value);
+        });
+        this.log.info(result + '');
     }
     private printHeader() {
         this.log.info(`
@@ -27,15 +44,17 @@ export class TaskRunner {
             C H E C K M A R X              
                                            
 Starting Checkmarx scan`);
-const taskObject = JSON.stringify(taskLib, null, 2)
-        let endpointId = taskLib.getInput('CheckmarxService', false) || '';
+    }
+
+    private initiateScanConfig() {
+        let endpointId = taskLib.getInput('CheckmarxService', true) || '';
         let astServerUrl = taskLib.getEndpointUrl(endpointId, false) || '';
         let astUsername = taskLib.getEndpointAuthorizationParameter(endpointId, 'username', false) || '';
         let astPassword = taskLib.getEndpointAuthorizationParameter(endpointId, 'password', false) || '';
-        this.log.info("BaseURI: " + astServerUrl);
-        this.log.info("Username: " + astUsername);
-        this.log.info("Password: " + astPassword);
-        this.log.info("TaskObject: " + taskObject);
+        this.cxScanConfig.baseUri = astServerUrl;
+        this.cxScanConfig.clientId=astUsername;
+        this.cxScanConfig.clientSecret=astPassword;
+        return this.cxScanConfig;
     }
 }
 
