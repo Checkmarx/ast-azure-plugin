c50555a2e000384a7eaad5ec1f80709c063304df
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-07-07 19:19:45 -0400

    successful case

--- CxASTScan/test/_suite.ts
+++ CxASTScan/test/_suite.ts
@@ -3,8 +3,20 @@ import * as path from 'path';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
 import CxScan from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxScan";
 import assert = require("assert");
+import {MockTestRunner} from "azure-pipelines-task-lib/mock-test";
 //import assert = require("assert");
 //import CxScan from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxScan";
+
+function runQueuedCase(tr: MockTestRunner) {
+    const temp = tr.stdout.split('\n');
+    if(isJsonString(temp[temp.length-2])) {
+        const val: CxScan = JSON.parse(temp[temp.length - 2]);
+        console.log(val);
+        assert.strictEqual(val.Status, "Queued");
+    }
+    //assert.strictEqual(tr.succeeded, true, 'should have succeeded');
+
+}
 
 describe('Task runner test', function () {
 
@@ -15,23 +27,19 @@ describe('Task runner test', function () {
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
         console.log(tr.succeeded);
-        const temp = tr.stdout.split('\n');
-        if(isJsonString(temp[temp.length-2])) {
-            const val: CxScan = JSON.parse(temp[temp.length - 2]);
-            console.log(val);
-            assert.strictEqual(val.Status, "Queued");
-        }
+        runQueuedCase(tr);
         done();
     });
 
     it('should be success wait mode', async function (done: Mocha.Done) {
-        this.timeout(1000);
+        this.timeout(300000);
         let tp = path.join(__dirname, 'success_waitmode.js');
         // let val:CxScan;
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
-        console.log(tr.stdout);
+        runQueuedCase(tr);
         done();
+
     });
 });
 
