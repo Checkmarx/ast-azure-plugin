2dbf18b9caddce43f05be182a4a94c2e61a7d1a0
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-07-07 15:09:23 +0100

    add no wait flag

--- CxASTScan/test/success.ts
+++ CxASTScan/test/success.ts
@@ -5,5 +5,6 @@ let taskPath = path.join(__dirname, '..', 'index.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 tmr.setInput("projectName", 'TestJayADO');
 tmr.setInput("enableSastScan", 'true');
+tmr.setInput("additionalParams", '--no-wait');
 
 tmr.run();
