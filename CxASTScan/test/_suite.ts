85a665c2ff89f601ae89c47d8988fb48b8157c78
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-07-29 11:49:49 +0100

    fix integration tests

--- CxASTScan/test/_suite.ts
+++ CxASTScan/test/_suite.ts
@@ -4,66 +4,59 @@ import CxScan from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxScan";
 import assert = require("assert");
 import {MockTestRunner} from "azure-pipelines-task-lib/mock-test";
 
-function runQueuedCase(tr: MockTestRunner) {
-    const temp = tr.stdout.split('\n');
-    if(isJsonString(temp[temp.length-2])) {
-        const val: CxScan = JSON.parse(temp[temp.length - 2]);
-        console.log(val);
-        return val;
-    }
-
-}
-
 describe('Task runner test', function () {
 
-    it('should be success no wait mode', function(done: Mocha.Done) {
+    it('should be success no wait mode', function() {
         this.timeout(300000);
         let tp = path.join(__dirname, 'success_nowait.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
         console.log(tr.succeeded);
-        const status = runQueuedCase(tr);
-        assert.deepStrictEqual(status.Status,"Queued");
-        done();
+        const scanObject = getScanObject(tr);
+        assert.deepStrictEqual(scanObject.Status,"Queued");
     });
 
-    it('should be success wait mode', async function (done: Mocha.Done) {
+    it('should be success wait mode',  function () {
         this.timeout(300000);
         let tp = path.join(__dirname, 'success_waitmode.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
-        const status = runQueuedCase(tr);
-        assert.deepStrictEqual(status.Status,"Queued");
+        const scanObject = getScanObject(tr);
+        assert.deepStrictEqual(scanObject.Status,"Queued");
         assert.ok(tr.succeeded);
-        done();
 
     });
 
-    it('should be failure additional params', async function (done: Mocha.Done) {
+    it('should be failure additional params', function () {
         this.timeout(300000);
         let tp = path.join(__dirname, 'failure_additional_params.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
-        const status = runQueuedCase(tr);
-        assert.deepStrictEqual(status.Status,"Queued");
         assert.ok(tr.failed);
-        done();
-
     });
 
-    it('should be failure preset', async function (done: Mocha.Done) {
+    it('should be failure preset', function () {
         this.timeout(300000);
         let tp = path.join(__dirname, 'failure_wrong_preset.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
-        const status = runQueuedCase(tr);
-        assert.deepStrictEqual(status.Status,"Queued");
+        const scanObject = getScanObject(tr);
+        assert.deepStrictEqual(scanObject.Status,"Queued");
         assert.ok(tr.failed);
-        done();
-
     });
 
 });
+
+function getScanObject(tr: MockTestRunner) : CxScan {
+    const logs = tr.stdout.split('\n');
+    const jsonString = logs.filter((log) => {
+        return log && isJsonString(log);
+    });
+    if (jsonString[0])  {
+        console.log("Scan json: " + jsonString);
+        return JSON.parse(jsonString[0]);
+    }
+}
 
 function isJsonString(s: string) {
     try {
