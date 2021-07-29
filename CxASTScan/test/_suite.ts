aa09333d533302631adc27935764837966614410
AndreGCX <87319846+AndreGCX@users.noreply.github.com>
2021-07-29 12:16:34 +0100

    some tests added and error is showed when additional params arent ok (#6)
    * Add return code and update javascript wrapper version
    * Some tests added and error is showed when additional params aren't ok

--- CxASTScan/test/_suite.ts
+++ CxASTScan/test/_suite.ts
@@ -1,51 +1,62 @@
 import * as path from 'path';
-//import * as assert from 'assert';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
+import {MockTestRunner} from 'azure-pipelines-task-lib/mock-test';
 import CxScan from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxScan";
 import assert = require("assert");
-import {MockTestRunner} from "azure-pipelines-task-lib/mock-test";
-//import assert = require("assert");
-//import CxScan from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/CxScan";
-
-function runQueuedCase(tr: MockTestRunner) {
-    const temp = tr.stdout.split('\n');
-    if(isJsonString(temp[temp.length-2])) {
-        const val: CxScan = JSON.parse(temp[temp.length - 2]);
-        console.log(val);
-        return val;
-        //assert.strictEqual(val.Status, "Queued");
-    }
-    //assert.strictEqual(tr.succeeded, true, 'should have succeeded');
-
-}
 
 describe('Task runner test', function () {
 
-    it('should be success no wait mode', function(done: Mocha.Done) {
+    it('should be success no wait mode', function () {
         this.timeout(300000);
         let tp = path.join(__dirname, 'success_nowait.js');
-       // let val:CxScan;
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
         console.log(tr.succeeded);
-        const status = runQueuedCase(tr);
-        assert.deepStrictEqual(status.Status,"Queued");
-        done();
+        const scanObject = getScanObject(tr);
+        assert.deepStrictEqual(scanObject.Status, "Queued");
     });
 
-    it('should be success wait mode', async function (done: Mocha.Done) {
+    it('should be success wait mode', function () {
         this.timeout(300000);
         let tp = path.join(__dirname, 'success_waitmode.js');
-        // let val:CxScan;
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
-        const status = runQueuedCase(tr);
-        assert.deepStrictEqual(status.Status,"Queued");
+        const scanObject = getScanObject(tr);
+        assert.deepStrictEqual(scanObject.Status, "Queued");
         assert.ok(tr.succeeded);
-        done();
 
     });
+
+    it('should be failure additional params', function () {
+        this.timeout(300000);
+        let tp = path.join(__dirname, 'failure_additional_params.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run();
+        assert.ok(tr.failed);
+    });
+
+    it('should be failure preset', function () {
+        this.timeout(300000);
+        let tp = path.join(__dirname, 'failure_wrong_preset.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run();
+        const scanObject = getScanObject(tr);
+        assert.deepStrictEqual(scanObject.Status, "Queued");
+        assert.ok(tr.failed);
+    });
+
 });
+
+function getScanObject(tr: MockTestRunner): CxScan {
+    const logs = tr.stdout.split('\n');
+    const jsonString = logs.filter((log) => {
+        return log && isJsonString(log);
+    });
+    if (jsonString[0]) {
+        console.log("Scan json: " + jsonString);
+        return JSON.parse(jsonString[0]);
+    }
+}
 
 function isJsonString(s: string) {
     try {
--- CxASTScan/test/failure_wrong_preset.ts
+++ CxASTScan/test/failure_wrong_preset.ts
@@ -4,7 +4,7 @@ import path = require('path');
 
 let taskPath = path.join(__dirname, '..', 'index.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
-tmr.setInput("projectName", 'TestADO_NoWait');
+tmr.setInput("projectName", 'Test_Wrong_Preset');
 tmr.setInput("zipFileFilter", '*.ts');
-tmr.setInput("additionalParams", '--nowait');
+tmr.setInput("additionalParams", '--sast-preset-name fake');
 tmr.run();
