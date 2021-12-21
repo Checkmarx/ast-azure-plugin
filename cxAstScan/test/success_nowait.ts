2582ebd8fb894b517a3fa9aed0a7eae5f0292454
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-12-21 11:43:56 +0000

    update tests

--- cxAstScan/test/success_nowait.ts
+++ cxAstScan/test/success_nowait.ts
@@ -1,9 +1,19 @@
 import tmrm = require('azure-pipelines-task-lib/mock-run');
 import path = require('path');
+import process from "process";
 
 
 let taskPath = path.join(__dirname, '..', 'index.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+tmr.setInput("tenantName", process.env.TENANT!);
+tmr.setInput("CheckmarxService", "cxauth");
+tmr.registerMockExport('getEndpointUrl', () => { return  process.env.BASE_URI!; });
+tmr.registerMockExport('getEndpointAuthorizationParameter', (endpoint, key) => {
+    if (key === 'username') return process.env.CLIENT_ID!
+    if (key === 'password') return process.env.CLIENT_SECRET!
+
+    return "";
+});
 tmr.setInput("projectName", 'TestADO_NoWait');
 tmr.setInput("branchName", 'dummy_branch');
 tmr.setInput("additionalParams", '--async --file-filter "*.ts"');
