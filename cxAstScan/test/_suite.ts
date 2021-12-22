371bef2d7cf13277cb1db8c39ce2e6c2cd45ab18
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-12-22 08:30:06 +0000

    update test

--- cxAstScan/test/_suite.ts
+++ cxAstScan/test/_suite.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
-import { MockTestRunner } from 'azure-pipelines-task-lib/mock-test';
 import * as assert from 'assert';
 
 
