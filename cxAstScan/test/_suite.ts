fad8019b0b72c349a88cc7c878acdcad890d6a37
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2021-12-23 09:21:31 +0000

    Update JavaScript wrapper to 0.0.40 (#96)
    * update wrapper to 0.40

--- cxAstScan/test/_suite.ts
+++ cxAstScan/test/_suite.ts
@@ -5,6 +5,30 @@ import * as assert from 'assert';
 
 describe('Task runner test', function () {
 
+    it('should be success wait mode', function (done) {
+        this.timeout(3000000);
+        const tp = path.join(__dirname, 'success_waitmode.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run(10);
+
+        console.log(tr.stdout)
+        console.log(tr.stderr)
+        assert.ok(tr.succeeded);
+        done();
+    });
+
+    it('should be success no wait mode', function (done) {
+        this.timeout(300000);
+        const tp = path.join(__dirname, 'success_nowait.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run(10);
+
+        console.log(tr.stdout)
+        console.log(tr.stderr)
+        assert.ok(tr.succeeded);
+        done();
+    });
+
     it('should be failure additional params', function (done) {
         this.timeout(300000);
         const tp = path.join(__dirname, 'failure_additional_params.js');
@@ -25,30 +49,6 @@ describe('Task runner test', function () {
         console.log(tr.stdout)
         console.log(tr.stderr)
         assert.ok(tr.failed);
-        done();
-    });
-
-    it('should be success no wait mode', function (done) {
-        this.timeout(300000);
-        const tp = path.join(__dirname, 'success_nowait.js');
-        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run(10);
-
-        console.log(tr.stdout)
-        console.log(tr.stderr)
-        assert.ok(tr.succeeded);
-        done();
-    });
-
-    it('should be success wait mode', function (done) {
-        this.timeout(3000000);
-        const tp = path.join(__dirname, 'success_waitmode.js');
-        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run(10);
-
-        console.log(tr.stdout)
-        console.log(tr.stderr)
-        assert.ok(tr.succeeded);
         done();
     });
 });
