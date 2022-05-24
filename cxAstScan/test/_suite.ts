a12f8ee6b7fee0df25c717e6b8663bd927eda56e
AndreGCX <andre.goncalves@checkmarx.com>
2022-05-24 11:45:40 +0100

    uncomm

--- cxAstScan/test/_suite.ts
+++ cxAstScan/test/_suite.ts
@@ -5,52 +5,52 @@ import * as assert from 'assert';
 
 describe('Task runner test', function () {
 
-    // it('should be success wait mode', function (done) {
-    //     this.timeout(3000000);
-    //     const tp = path.join(__dirname, 'success_waitmode.js');
-    //     const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-    //     tr.run(10);
+    it('should be success wait mode', function (done) {
+        this.timeout(3000000);
+        const tp = path.join(__dirname, 'success_waitmode.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run(10);
 
-    //     console.log(tr.stdout)
-    //     console.log(tr.stderr)
-    //     assert.ok(tr.succeeded);
-    //     done();
-    // });
+        console.log(tr.stdout)
+        console.log(tr.stderr)
+        assert.ok(tr.succeeded);
+        done();
+    });
 
-    // it('should be success no wait mode', function (done) {
-    //     this.timeout(300000);
-    //     const tp = path.join(__dirname, 'success_nowait.js');
-    //     const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-    //     tr.run(10);
+    it('should be success no wait mode', function (done) {
+        this.timeout(300000);
+        const tp = path.join(__dirname, 'success_nowait.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run(10);
 
-    //     console.log(tr.stdout)
-    //     console.log(tr.stderr)
-    //     assert.ok(tr.succeeded);
-    //     done();
-    // });
+        console.log(tr.stdout)
+        console.log(tr.stderr)
+        assert.ok(tr.succeeded);
+        done();
+    });
 
-    // it('should be failure additional params', function (done) {
-    //     this.timeout(300000);
-    //     const tp = path.join(__dirname, 'failure_additional_params.js');
-    //     const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-    //     tr.run(10);
-    //     console.log(tr.stdout)
-    //     console.log(tr.stderr)
-    //     assert.ok(tr.failed);
-    //     done();
-    // });
+    it('should be failure additional params', function (done) {
+        this.timeout(300000);
+        const tp = path.join(__dirname, 'failure_additional_params.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run(10);
+        console.log(tr.stdout)
+        console.log(tr.stderr)
+        assert.ok(tr.failed);
+        done();
+    });
 
-    // it('should be failure preset', function (done) {
-    //     this.timeout(300000);
-    //     const tp = path.join(__dirname, 'failure_wrong_preset.js');
-    //     const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-    //     tr.run(10);
+    it('should be failure preset', function (done) {
+        this.timeout(300000);
+        const tp = path.join(__dirname, 'failure_wrong_preset.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run(10);
 
-    //     console.log(tr.stdout)
-    //     console.log(tr.stderr)
-    //     assert.ok(tr.failed);
-    //     done();
-    // });
+        console.log(tr.stdout)
+        console.log(tr.stderr)
+        assert.ok(tr.failed);
+        done();
+    });
 
     it('should be success no cancel scan', function (done) {
         this.timeout(300000);
